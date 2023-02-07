32b52258154ddd8709ddc030c871fe4e9b0ccb5a
Abraham Dulla <abrish.d143t@gmail.com>
2023-02-07 12:38:08 +0300

    for loop

---
+++ 2control-flow/2loop/1for-loop.py
@@ -0,0 +1,17 @@
+# a loop is a control structure that allows you to repeatedly execute
+# a block of code while a certain condition is true.
+
+# A for loop allows you to iterate over a sequence (such as a list, tuple, or string)
+# and execute a block of code for each item in the sequence.
+# In Python, there are two main types of loops: for loops and while loops.
+
+# Sample list of numbers
+numbers = [1, 2, 3, 4, 5]
+# Iterate over the list using for loop
+for num in numbers:
+    print("Loop", num)
+
+
+numbers = 5
+for num in range(5):
+    print("Number", num)
