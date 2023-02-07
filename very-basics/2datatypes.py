12b19ddb760b8039f1fdd90d3d78301d6028718c
Abraham Dulla <abrish.d143t@gmail.com>
2023-02-07 11:11:33 +0300

    variables

--- very-basics/2datatypes.py
+++ very-basics/2datatypes.py
@@ -1,4 +1,38 @@
-name = "Abraham"
-age = 26
+
 
 print("My name is ", name, "and i am ",  age, "years old")
+
+
+# In Python, there are several built-in data types:
+
+# 1 Numeric data types:
+# int (integers)
+age = 26
+# float (floating-point numbers)
+height = 1.68
+# complex (complex numbers)
+
+
+# 2 Sequential data types:
+# str (string)
+name = "Abraham"
+# list (ordered collection of items)
+# tuple (immutable ordered collection of items)
+
+# 3 Mapping data type:
+# dict (unordered collection of key-value pairs)
+
+# 4 Set data types:
+# set (unordered collection of unique items)
+# frozenset (immutable unordered collection of unique items)
+
+# 5 Boolean data type:
+# bool (True or False)
+
+# 6 Binary data types:
+# bytes (immutable sequence of bytes)
+# bytearray (mutable sequence of bytes)
+
+# Others:
+# None (special constant used to denote the absence of a value)
+# Ellipsis (special constant used in slicing operations)
