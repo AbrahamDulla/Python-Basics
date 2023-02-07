f598e28dfc154ee3cc3d7535d9dfa0be3c00f8f3
Abraham Dulla <abrish.d143t@gmail.com>
2023-02-07 12:44:10 +0300

    while loop

---
+++ 2control-flow/2loop/2while-loop.py
@@ -0,0 +1,9 @@
+# A while loop, on the other hand, will repeat
+# a block of code as long as a certain condition is true.
+
+count = 0
+to_count = 10
+
+while (count < to_count):
+    print("count...", count)
+    count += 1
